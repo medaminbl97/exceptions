b83f17c1e4d90129013a579a2c3ccc53b08d09c5
medaminbl97 <mbellil262@gmail.com>
2022-04-12 14:39:04 +0200

    project created

---
+++ cmake-build-debug/CMakeFiles/exeptions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exeptions.dir/main.cpp.o"
+  "exeptions"
+  "exeptions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exeptions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/exeptions.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for exeptions.
+# This may be replaced when dependencies are built.
