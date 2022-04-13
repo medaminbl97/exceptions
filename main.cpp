7c851a1cc2d233760a680d5df6a2336c6dc9927e
medaminbl97 <mbellil262@gmail.com>
2022-04-13 11:15:36 +0200

    fertig

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+void func(){
+
+    int a , b , c ;
+    std::cout << "enter a : " ; std::cin >> a;
+    std::cout << "enter b : " ; std::cin >> b;
+    if(b==0) throw "a/b geht nicht weil b = 0 !";
+    else std::cout << "a/b = " << a/b << std::endl;
+
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    while(true){
+
+     try{
+         func();
+     }
+     catch (const char* msg)
+     {
+         std::cout << msg << std::endl;
+     }
+    }
     return 0;
 }
